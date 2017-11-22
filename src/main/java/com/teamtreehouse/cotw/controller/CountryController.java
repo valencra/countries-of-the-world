71f74c76c9e7bbe7e8be9bf8c7e700ebef6c3961
valencra <valencra@mcmaster.ca>
2017-11-21 22:52:58 -0500

    Added country controller

---
+++ src/main/java/com/teamtreehouse/cotw/controller/CountryController.java
@@ -0,0 +1,32 @@
+package com.teamtreehouse.cotw.controller;
+
+import com.teamtreehouse.cotw.data.CountryRepository;
+import com.teamtreehouse.cotw.model.Country;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+public class CountryController {
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    @RequestMapping("/")
+    public String getCountries(ModelMap modelMap) {
+        List<Country> countries = countryRepository.getCountries();
+        modelMap.put("countries", countries);
+        return "index";
+    }
+
+    @RequestMapping("/countries/{name}")
+    public String getCountry(@PathVariable String name, ModelMap modelMap) {
+        Country country = countryRepository.getCountry(name);
+        modelMap.put("country", country);
+        return "detail";
+    }
+}
